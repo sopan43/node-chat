336784ec9ad9b1b5d3ca53a5f553dea9c6f9afd1
Sopan <sopanmittal43@gmail.com>
2017-07-22 15:56:56 +0530

    Add message event

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+		socket.on('connect',function () {
+			console.log('Connect to the server')
+		
+			socket.emit('createMessage', {
+				from: 'sopan',
+				text: 'some text'
+			});
+		});
+
+		socket.on('disconnect', function()  {
+				console.log('disconnect from the server')		
+			});
+
+	socket.on('newMessage', function(message) {
+		console.log('New Message', message);
+	});
